60119c589818bc5fab98a6ba4b4e18fbd63b826f
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-06-06 13:31:36 -0500

    add Node struct

---
+++ list/linkedList/node.go
@@ -0,0 +1,31 @@
+package linkedlist
+
+type Node[T any] struct {
+	data     T
+	nextNode *Node[T]
+}
+
+func (node *Node[T]) GetData() *T {
+	return &node.data
+}
+
+func (node *Node[T]) SetData(data *T) {
+	node.data = *data
+}
+
+func (node *Node[T]) GetNext() *Node[T] {
+	return node.nextNode
+}
+
+func (node *Node[T]) SetNext(nextNode *Node[T]) {
+	node.nextNode = nextNode
+}
+
+func NewNode[T any](data T) *Node[T] {
+	node := Node[T]{
+		data:     data,
+		nextNode: nil,
+	}
+
+	return &node
+}
