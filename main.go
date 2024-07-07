ef2e922dba3b7c9d1c3ed3568ce20392ed23a287
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-07-07 14:33:07 -0500

    clear test in main

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	linkedlist "structures/list/linkedList"
-)
-
 func main() {
-	node := linkedlist.NewNode(2)
-
-	println(*node.GetData())
-
-	num := 3
-
-	node.SetData(&num)
-
-	println(*node.GetData())
-
-	node2 := linkedlist.NewNode(4)
-
-	node.SetNext(node2)
-
-	println(*node.GetNext().GetData())
 
 }
